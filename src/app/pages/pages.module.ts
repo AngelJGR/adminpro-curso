f78faff2a90bfa50e11144431082c45a60e3a8d2
Angel Guerrero <ajguerreror@gmail.com>
2021-05-04 17:07:10 -0400

    Progress dinamico e importacion de formsModule

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { AppRoutingModule } from '../app-routing.module';
+import { FormsModule } from '@angular/forms';
 
 import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
@@ -19,8 +20,9 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   ],
   imports: [
     CommonModule,
+    FormsModule,
     SharedModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   exports: [
     DashboardComponent,
--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-progress',
   templateUrl: './progress.component.html',
   styleUrls: [ './progress.component.css' ]
 })
-export class ProgressComponent implements OnInit {
+export class ProgressComponent {
+
+  progress: number = 10;
 
   constructor() { }
 
-  ngOnInit(): void {
+  get getProgress () {
+    return `${this.progress}%`;
+  }
+
+  changeValueProgress(val: number) {
+    if (this.progress >= 100 && val >= 0) {
+      return this.progress = 100;
+    }
+    if (this.progress <= 0 && val < 0) {
+      return this.progress = 0;
+    }
+    return this.progress = this.progress + val;
   }
 
 }
