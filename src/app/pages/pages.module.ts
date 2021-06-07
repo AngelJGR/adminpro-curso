ade677d54b68708ff556619f8eac38b7e2dc22ee
Angel Guerrero <ajguerreror@gmail.com>
2021-06-07 16:49:44 -0400

    Añade components module

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+//Modules
 import { SharedModule } from '../shared/shared.module';
 import { AppRoutingModule } from '../app-routing.module';
-import { FormsModule } from '@angular/forms';
+import { ComponentsModule } from '../components/components.module';
 
 import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
@@ -23,6 +26,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     FormsModule,
     SharedModule,
     AppRoutingModule,
+    ComponentsModule
   ],
   exports: [
     DashboardComponent,
