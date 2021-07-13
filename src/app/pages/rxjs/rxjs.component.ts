993c421a0694063b29efa3b6fea987febd59f800
Angel Guerrero <ajguerreror@gmail.com>
2021-07-13 18:16:35 -0400

    Class interval, operators take y map

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
-import { retry } from 'rxjs/operators';
+import { Observable, interval } from 'rxjs';
+import { retry, take, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-rxjs',
@@ -12,12 +12,27 @@ export class RxjsComponent {
 
   constructor() {
     
-    this.returnObservable().pipe(
-      retry(1)
-    ).subscribe(
-      valor => console.log('Subs:', valor), 
-      error => console.warn('Error', error),
-      () => console.info('Finalizado'));
+    // this.returnObservable().pipe(
+    //   retry(1)
+    // ).subscribe(
+    //   valor => console.log('Subs:', valor), 
+    //   error => console.warn('Error', error),
+    //   () => console.info('Finalizado'));
+
+    this.returnInterval().subscribe( console.log )
+
+    // this.returnInterval().subscribe(
+    //   valor => console.log(valor)
+    // )
+  }
+
+  returnInterval(): Observable<number>{
+    return interval(1000)
+      .pipe( 
+        take(4),
+        map(val => val+1)
+      );
+    
   }
 
   returnObservable(): Observable<number> {
