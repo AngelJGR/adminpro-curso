bbe501ca49789be0e32f2c919b637cc6bf2a9e82
Angel Guerrero <ajguerreror@gmail.com>
2023-02-05 16:50:14 -0400

    paginate users

--- src/app/pages/maintenances/users/users.component.ts
+++ src/app/pages/maintenances/users/users.component.ts
@@ -12,17 +12,32 @@ export class UsersComponent implements OnInit {
 
   public users: Array<User> = [];
   public totalUsers: number = 0;
+  public from: number = 0;
 
   constructor(
     private userService: UserService
   ) { }
 
   ngOnInit(): void {
-    this.userService.getUsers()
-    .subscribe(({users, totalUsers}) => {
-      this.users = users;
-      this.totalUsers = totalUsers;
-    });
+    this.getUser()
+  }
+
+  getUser() {
+    this.userService.getUsers(this.from)
+      .subscribe(({users, totalUsers}) => {
+        this.totalUsers = totalUsers;
+        this.users = users;
+      });
+  }
+
+  changePage (value: number) {
+    this.from += value
+    if (this.from < 0)
+      this.from = 0
+    else if (this.from > this.totalUsers)
+      this.from -= value
+    
+    this.getUser()
   }
 
 }
