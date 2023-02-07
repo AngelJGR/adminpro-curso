9c6cba22aab7e2f9efc887220c899c27cc33d977
Angel Guerrero <ajguerreror@gmail.com>
2023-02-07 14:26:30 -0400

    Designing modal

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -6,16 +6,17 @@ import { ChartsModule } from 'ng2-charts';
 
 import { IncrementadorComponent } from './incrementador/incrementador.component';
 import { DonaComponent } from './dona/dona.component';
+import { ModalImageComponent } from './modal-image/modal-image.component';
 
 
 
 @NgModule({
-  declarations: [IncrementadorComponent, DonaComponent],
+  declarations: [IncrementadorComponent, DonaComponent, ModalImageComponent],
   imports: [
     CommonModule,
     FormsModule,
     ChartsModule
   ],
-  exports: [IncrementadorComponent, DonaComponent]
+  exports: [IncrementadorComponent, DonaComponent, ModalImageComponent]
 })
 export class ComponentsModule { }
