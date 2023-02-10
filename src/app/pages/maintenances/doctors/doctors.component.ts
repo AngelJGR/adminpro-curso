14b0dd2f345eadb2b8636c5270985cd442f5b339
Angel Guerrero <ajguerreror@gmail.com>
2023-02-10 19:40:39 -0400

    List doctors

--- src/app/pages/maintenances/doctors/doctors.component.ts
+++ src/app/pages/maintenances/doctors/doctors.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Doctor } from 'src/app/models/doctor.model';
+import { DoctorService } from 'src/app/services/doctor.service';
+import { ModalImageService } from 'src/app/services/modal-image.service';
 
 @Component({
   selector: 'app-doctors',
@@ -8,9 +11,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DoctorsComponent implements OnInit {
 
-  constructor() { }
+  public doctors: Doctor[] = []
+  public totalHospitals: number
+  public loading: boolean = true
+
+  constructor(
+    private doctorService: DoctorService,
+  ) { }
 
   ngOnInit(): void {
+    this.getDoctors()
+  }
+
+  getDoctors() {
+    this.loading = true
+    this.doctorService.getDoctors()
+      .subscribe(res => {
+        this.doctors = res.doctors
+        this.loading = false;
+      })
   }
 
 }
