c76014860be544b153e958affbe51ef8d4fe33d5
Angel Guerrero <ajguerreror@gmail.com>
2023-02-10 19:01:42 -0400

    Doctor component, model and service

---
+++ src/app/models/doctor.model.ts
@@ -0,0 +1,19 @@
+import { Hospital } from "./hospital.model";
+
+interface _DoctorUser{
+    _id: string;
+    name: string;
+    img: string;
+}
+
+export class Doctor {
+
+    public name: string;
+    public _id?: string;
+    public img?: string;
+    public user?: _DoctorUser;
+    public hospital: Hospital
+
+    constructor(
+  ) { }
+}
