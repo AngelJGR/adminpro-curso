d1bee4a2ce6678fc3aad10ce086e1f224dba15c1
Angel Guerrero <ajguerreror@gmail.com>
2023-02-13 18:34:59 -0400

    Form control

--- src/app/pages/maintenances/doctors/doctor.component.ts
+++ src/app/pages/maintenances/doctors/doctor.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Hospital } from 'src/app/models/hospital.model';
+import { HospitalService } from 'src/app/services/hospital.service';
 
 @Component({
   selector: 'app-doctor',
@@ -8,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DoctorComponent implements OnInit {
 
-  constructor() { }
+  public doctorForm: FormGroup
+  public hospitals: Hospital[] = []
+
+  constructor(
+    public fb: FormBuilder,
+    private hospitalService: HospitalService
+  ) { }
 
   ngOnInit(): void {
+    this.doctorForm = this.fb.group({
+      name: ['Nombre', Validators.required],
+      hospital: ['', Validators.required]
+    })
+    this.getHospitals()
+  }
+
+  saveDoctor(){
+    console.log(this.doctorForm.value)
+  }
+
+  getHospitals() {
+    this.hospitalService.getHospitals()
+      .subscribe(res => {
+        console.log(res)
+        this.hospitals = res.hospitals
+      } )
   }
 
 }
